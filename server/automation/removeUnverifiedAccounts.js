80f969826c109da8c8626e13863bf46940f27423
Roshan Singh <107341705+roshan9641@users.noreply.github.com>
2025-05-02 16:16:08 +0530

    Update removeUnverifiedAccounts.js

--- server/automation/removeUnverifiedAccounts.js
+++ server/automation/removeUnverifiedAccounts.js
@@ -1,5 +1,5 @@
 import cron from 'node-cron';
-import {User} from "../models/usermodel.js";
+import {User} from "../models/userModel.js";
 
 export const removeUnverifiedAccounts = () =>{
     cron.schedule("*/30 * * * *", async () => {
