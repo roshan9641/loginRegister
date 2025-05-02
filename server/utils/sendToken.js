1ef7532a0323fec69d35a7a809d161228d4a048d
Roshan Singh <107341705+roshan9641@users.noreply.github.com>
2025-05-02 23:51:17 +0530

    Update sendToken.js

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,13 +1,18 @@
-export const sendToken= (user,statusCode,message,res)=>{
-    const token  = user.generateToken();
-    res.status(statusCode).cookie("token",token,{
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-        httpOnly:true,
-})
-.json({
-    success: true,
-    message,
-    token,
-    user
+export const sendToken = (user, statusCode, message, res) => {
+  const token = user.generateToken();
+
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: true,             // Required on HTTPS (Render)
+      sameSite: "None",         // Needed for cross-origin cookies
+    })
+    .json({
+      success: true,
+      message,
+      token,
+      user,
     });
 };
