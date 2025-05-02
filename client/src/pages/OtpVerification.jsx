80c63e8e02bdb0e2ea2d5ba84196f8b0fd6422d3
Roshan Singh <107341705+roshan9641@users.noreply.github.com>
2025-05-02 21:12:22 +0530

    Update OtpVerification.jsx

--- client/src/pages/OtpVerification.jsx
+++ client/src/pages/OtpVerification.jsx
@@ -36,7 +36,7 @@ const OtpVerification = () => {
       phone,
     };
     await axios
-      .post("http://localhost:3000/api/v1/user/otp-verification", data, {
+      .post("https://loginregister-e0bb.onrender.com/api/v1/user/otp-verification", data, {
         withCredentials: true,
         headers: { "Content-Type": "application/json" },
       })
